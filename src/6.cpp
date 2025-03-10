ed822f41d321d562279df6bc19b8872548ce5509
lumumbaelder128 <lumumbaelder128@gmail.com>
2025-03-10 00:21:09 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1 = 5;
+    int num2 = 8;
+    int sum = num1 + num2;
+    cout << "The sum of 5 and 8 is: " << sum << endl;
+    return 0;
+}
