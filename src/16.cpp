3c31ca8eb1c69788c5f532b190cb53f16b807628
lumumbaelder128 <lumumbaelder128@gmail.com>
2025-03-20 08:50:57 +0000

    Add 16.cpp

---
+++ src/16.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 7;
+  int z = (x + y) * (x - y);
+  cout << "The value of z is: " << z << endl;
+  return 0;
+}
