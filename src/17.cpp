ab5abff62224e3f068e1ba981cdbfb09cc9836b7
lumumbaelder128 <lumumbaelder128@gmail.com>
2025-03-21 10:09:23 +0000

    Add 17.cpp

---
+++ src/17.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int result = a * b;
+    cout << "The result of " << a << " multiplied by " << b << " is: " << result << endl;
+    return 0;
+}
