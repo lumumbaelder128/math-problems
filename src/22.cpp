60130a47bb97473a50c21d1326b4559bf23aa49d
lumumbaelder128 <lumumbaelder128@gmail.com>
2025-03-30 06:48:09 +0000

    Add 22.cpp

---
+++ src/22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    
+    if (a > b) {
+        cout << "a is greater than b." << endl;
+    } else {
+        cout << "b is greater than a." << endl;
+    }
+    
+    return 0;
+}
